a58192ecea6ffe4267b79d69a17836200965f048
Alejrot <marotmarcelo@gmail.com>
2025-04-01 00:26:24 -0300

    Parallel processing with treads added

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from time import time
 from pathlib import Path
 
+from multiprocessing import Pool
+from threading import Thread
+
+import psutil
+
+
 from PIL import Image
+
+
+
+import cv2
+
+
+
+def thread_convert_image(src_path, dst_path, quality):
+
+    print(f"{src_path}")
+
+    # PILLOW
+    im = Image.open(src_path)
+    im.save(dst_path, quality=quality)
+
+    # # OPENCV
+    # img = cv2.imread(src_path)
+    # guardado_exitoso = cv2.imwrite(dst_path, img)
+    # del img
 
 
 
@@ -30,6 +55,8 @@ src_paths = list(src_paths)
 start = time()
 
 
+threads_list = []
+
 for path in src_paths:
 
     # creating destiny path
@@ -37,11 +64,29 @@ for path in src_paths:
     dst_name = src_path.name
     dst_path = Path(dst_dir).joinpath(dst_name)
 
-    # converting image
-    im = Image.open(path)
-    im.save(dst_path, quality=quality)
+    # converting images in parallel
+    args = (path, dst_path, quality,)
+    conv_thread = Thread(
+        target=thread_convert_image,
+        args  =args
+        ) 
+
+    conv_thread.start()
+    threads_list.append(conv_thread)
+
+
+
+# awaits for conversion's end
+for thread in threads_list:
+    thread.join()
 
 
 end = time()
 
+
+
 print(f"Elapsed time: {(end-start)*1000 :7.6} mseg")
+
+
+
+
