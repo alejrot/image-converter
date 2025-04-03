8cbec8bc7ce5cd8a7ab14e36aa175746ef553508
Alejrot <marotmarcelo@gmail.com>
2025-04-02 21:58:59 -0300

    RGBA images support added

--- code/processing.py
+++ code/processing.py
@@ -1,19 +1,33 @@
 from pathlib import Path
 from threading import Thread
 
+# conversion with Pillow
 from PIL import Image
 
 
 def thread_convert_image(src_path, dst_path, quality:int=95):
     """This thread saves the source image in the destiny path after its conversion.
     Quality is a percentage that defines compression: a high percentage means minimal quality loss.
+    If source image is 4-channel (RGBA) then the output will be converted to 3-channel (RGB). 
     """
     try:
-        # conversion with Pillow
-        im = Image.open(src_path)
-        im.save(dst_path, quality=quality)
+        with Image.open(src_path) as im:
+            # RGB and monochrome images are converted directly
+            im.save(dst_path, quality=quality)
+
     except:
-        print(f"Image: {src_path} - unsupported image")
+        with Image.open(src_path) as im:    
+            # RGBA images are converted deleting transparency channel
+            source = im.split()
+            if len(source) == 4:
+                r,g,b,a = source
+                # A channel discarded
+                im = Image.merge("RGB", (r,g,b))
+                # im.save("9dzign_flame.jpg", quality=quality)
+                im.save(dst_path, quality=quality)
+                print(f"Image: {src_path} - transparency channel discarded")
+            else:
+                print(f"Image: {src_path} - unsupported image")
 
 
 
